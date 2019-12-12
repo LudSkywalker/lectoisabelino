3fa33e3dec0eeba8ca0f0580a065b0e48f0d47de
Lud Skywalker <ludingnumpaque@gmail.com>
2019-12-12 18:13:46 -0500

    ultimo

---
+++ plantillas/Dashio/ini.php
@@ -0,0 +1,27 @@
+
+
+    <section id="main-content">
+      <section class="wrapper">
+          <h1><i class="fa fa-angle-right"></i> Bienvenido </h1>
+        <div class="row content-panel mt mb">
+          <div class="col-md-6">
+            <h2 contenteditable="true">Has accedido a LectoIsabelino</h2>
+            <h3 contenteditable="true">Este es un aplicativo web el cual te permitira echar un pequeño vistazo a la biblioteca institucional, si eres bibliotecario o administrador tambien podras gestionar los diferentes recursos que esta ofrece </h3>
+            <h4 contenteditable="true">Colegio Parroquial Santa Isabel de Hungria</h4>
+          </div>
+          <div class="col-md-6">
+            <p contenteditable="true" class="mt">El colegio lleva años ofreciendo prestamos de libros y elementos a todos los miembros de la institucion, sin embargo esto funcionaba bajo un sistema algo arcaico y aunque con el pasar de los años se fueron implementando sistemas como las tablas de excel, este manejo sige siendo muy poco eficientes.</p>
+            <p contenteditable="true">Es por esto que nosotros decidimos crear una solucion, no hay nada mejor que aprovechar las nuevas tecnologias, entonces decidimos diseñar una pagina web capaz de automatizar este sistema y de poder llevar la informacion acerca de todos los recursos a la casa de cada uno de los miemiembros del colegio.</p>
+            <p contenteditable="true">LectoIsabelino nace como una alternativa mucho mas practica y util, diseñada especificamente para suplir con todas las necesidades de la institucion.</p>
+            <p contenteditable="true">Los encargados de realizar este diseño son el grupo White Hats: Laura Gaitan, Carlos Barrera, Edward Guavita y Valentina Lopez liderados por Luding Castañeda; Aprendices del SENA del tecnico en programacion de software .</p>
+          </div>
+        </div>
+        
+      </section>
+
+</section>
+<?php
+if (isset($erroresValidacion))
+    $erroresValidacion = NULL;
+?>
+
